ff2346803c421d5e2c4c0b02e7dd7910c37e9083
DilnaBiju <dilnabiju72@gmail.com>
2025-03-12 09:50:04 +0530

    deleted

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:palette_generator/palette_generator.dart';
 import 'dart:io';
